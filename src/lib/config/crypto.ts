eb8a060da00c47c692506e6603003ed1f5a60ef5
desiba <desiba_1@yahoo.com>
2025-03-17 09:40:16 +0100

    update encryption key

--- src/lib/config/crypto.ts
+++ src/lib/config/crypto.ts
@@ -1,11 +1,11 @@
 const CryptoJS = require('crypto-js');
 
 export const encrypt = async (email: string) => {
-    return CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(email), 'secret').toString(); 
+    return CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(email), process.env.ENCRYPTION_KEY).toString(); 
 }
 
 export const decrypt = async (ciphertext: string) => {
-    return CryptoJS.AES.decrypt(ciphertext, 'secret').toString(CryptoJS.enc.Utf8);
+    return CryptoJS.AES.decrypt(ciphertext, process.env.ENCRYPTION_KEY).toString(CryptoJS.enc.Utf8);
 }
 
   
